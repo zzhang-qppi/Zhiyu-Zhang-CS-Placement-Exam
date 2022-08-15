22454eac8f010d73cf5fe349c664292a5fa14df7
Zhiyu Zhang <zzhang.qppi@gmail.com>
2022-08-15 12:04:49 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import urllib.request
+import numpy as np
+
+#url = sys.agrv[1]
+url = "http://cs.carleton.edu/faculty/dln/placement/grid.txt"
+f = urllib.request.urlopen(url)
+list = []
+for line in f:
+    line_as_array = np.array(line.split(), dtype="int64")
+    list.append(line_as_array)
+data = np.array(list)
+print(data)
+f.close()
