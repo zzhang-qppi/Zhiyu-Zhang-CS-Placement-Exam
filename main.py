20f3441039203f42818c0c1f96e77e95130aeefa
Zhiyu Zhang <zzhang.qppi@gmail.com>
2022-08-16 06:49:15 +0800

    Final

--- main.py
+++ main.py
@@ -2,12 +2,40 @@ import sys
 import urllib.request
 import numpy as np
 
-#url = sys.agrv[1]
-url = "http://cs.carleton.edu/faculty/dln/placement/grid.txt"
+url = sys.argv[1]
 f = urllib.request.urlopen(url)
 list = []
 for line in f:
     line_as_array = np.array(line.split(), dtype="int64")
     list.append(line_as_array)
-data = np.array(list)
+num_grid = np.array(list)
 f.close()
+
+def linear_mult(data):
+    a = data[...,0:-3]
+    b = data[...,1:-2]
+    c = data[...,2:-1]
+    d = data[...,3:]
+    p = a*b*c*d
+#    assert(p.shape[0] == data.shape[0])
+#    assert(p.shape[1] == data.shape[1]-3)
+    return np.max(p)
+
+def diagonal_mult(data):
+    a = data[0:-3,0:-3]
+    b = data[1:-2,1:-2]
+    c = data[2:-1,2:-1]
+    d = data[3:,3:]
+    p = a*b*c*d
+#    assert(p.shape[0] == data.shape[0]-3)
+#    assert(p.shape[1] == data.shape[1]-3)
+    return np.max(p)
+
+print(max(
+    linear_mult(num_grid),
+    linear_mult(num_grid.T[::-1]),
+    diagonal_mult(num_grid),
+    diagonal_mult(num_grid.T[::-1])
+))
+
+            
